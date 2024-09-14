4971750a721767955f1bfd1bb1be44d83959c9ba
Oleg D <dolegan@gmail.com>
2024-09-13 22:23:41 -0400

    different attepmts to fix the logging issue

--- adaptive-pipeline-model-function/main.py
+++ adaptive-pipeline-model-function/main.py
@@ -66,7 +66,7 @@ def adaptive_pipeline_model_function(event, context):
     max_run_duration.seconds = 3600  # Set the duration to 3600 seconds (1 hour)
 
     # Generate the batch job configuration as a Python dictionary
-    batch_job_config_dict = {        
+    batch_job_config_dict = {
         'task_groups': [{
             'task_spec': {
                 'runnables': [{
@@ -75,17 +75,25 @@ def adaptive_pipeline_model_function(event, context):
                         'commands': []  # Add any necessary commands here
                     }
                 }],
-                'max_run_duration': max_run_duration # Set the maximum run duration
+                'max_run_duration': max_run_duration  # Set the maximum run duration
             },
             'task_count': 1,
             'parallelism': 1
         }],
-        'allocation_policy': {
+        'allocation_policy': {  # Updated allocation policy
             'instances': [{
-                'policy': {}
+                'policy': {
+                    'machineType': 'e2-highcpu-2',
+                    'provisioningModel': 'STANDARD'
+                }
             }]
+        },
+        'logs_policy': {
+            'destination': 'CLOUD_LOGGING'  # Ensure logs are sent to Cloud Logging
         }
     }
+
+    
 
     # Try deleting the previous job if it exists
     # try:
@@ -116,7 +124,7 @@ def adaptive_pipeline_model_function(event, context):
         while retry_count < max_retries:
             try:
                 job_status = client.get_job(name=job_name)
-                logger.debug(f"Job status: {job_status.status.state}")
+                logger.debug(f"Job status after {(5 ** (2 ** retry_count))} seconds: {job_status.status.state}")
                 # break  # Exit the loop if the job is found
                 time.sleep(5 * (2 ** retry_count))  # Exponential backoff (5s, 10s, 20s, etc.)
                 retry_count += 1
